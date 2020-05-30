692e7d702ccb3c1c341e22292efb051d24c96df1
Andrew <andrewchen118@gmail.com>
2020-05-29 22:32:09 -0700

    read maze f

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const fs = require("fs");
 
 function readMaze() {
+    var maze = [];
     fs.readFile("maze.txt", "utf-8", (err, data) => {
         if (err) throw err;
 
         // Converting Raw Buffer to text
         // data using tostring function.
-        console.log(data);
+        var array = [];
+        for (var i = 0; i < data.length; i++){
+            if ( data.charAt(i) == "\n" ) {
+                maze.push(array);
+                array = [];
+            } else {
+                if (data.charAt(i) != "\r") {
+                    array.push(data.charAt(i));
+                }
+            }
+        }
+        calculateMaze(maze);
     });
+}
+
+function calculateMaze(s) {
+    console.log(s);
 }
 
 readMaze();
