cede87e12904eeb87180eaaa9b2b58ae92026faf
Justin Chang <changguangyu@yahoo.com>
2020-05-31 01:01:50 -0700

    PriorityQueue compare function with nodes

--- src/dijkstra.js
+++ src/dijkstra.js
@@ -3,22 +3,40 @@ const TinyQueue = require("tinyqueue");
 var methods = {
     dijkstraCalculateMaze: function (maze, start, end) {
         var open = new TinyQueue(
-            new Node(start.x, start.y, 0, start),
+            [
+                new Node(
+                    start.x,
+                    start.y,
+                    0,
+                    new Node(start.x, start.y, 0, null)
+                ),
+            ],
             function (a, b) {
                 return a.weight - b.weight;
             }
         ); // create empty array of pre-evaluated positions
-        var closed = [new Node(start.x, start.y, 0, start)]; // create array of evaluated positions
+        var closed = [
+            new Node(start.x, start.y, 0, new Node(start.x, start.y, 0, null)),
+        ]; // create array of evaluated positions
+
+        var sn = new Node(
+            start.x,
+            start.y,
+            0,
+            new Node(start.x, start.y, 0, null)
+        );
+        console.log(pqContains(open, sn));
+
         open.pop(); // Pops the temporary node
 
-        var current = start;
-        while (current != end) {
-            var neighbors = getNeighbors(maze, current);
-            for (var i = 0; i < neighbors.length; i++) {
-                var node = new Node(neighbors[i].x, neighbors[i].y, 1, current);
-                //if()
-            }
-        }
+        // var current = start;
+        // while (current != end) {
+        //     var neighbors = getNeighbors(maze, current);
+        //     for (var i = 0; i < neighbors.length; i++) {
+        //         var node = new Node(neighbors[i].x, neighbors[i].y, 1, current);
+        //         //if()
+        //     }
+        // }
     },
 };
 
@@ -48,9 +66,10 @@ function getNeighbors(maze, current) {
 
 function pqContains(pq, node) {
     var items = pq.data;
-    for(var i = 0;i<items.length;i++){
-        if()
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].compare(node)) return true;
     }
+    return false;
 }
 
 class Node {
@@ -59,6 +78,18 @@ class Node {
         this.y = Y;
         this.weight = W;
         this.parent = P;
+    }
+
+    compare(node) {
+        if (
+            this.x == node.x &&
+            this.y == node.y &&
+            this.weight == node.weight &&
+            this.parent.x == node.parent.x &&
+            this.parent.y == node.parent.y
+        )
+            return true;
+        return false;
     }
 }
 
